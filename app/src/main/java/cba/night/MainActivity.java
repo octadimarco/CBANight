8dfe3104552a4071912c46322aec8623146880bd
Octavio <octa.dimarco@gmail.com>
2018-01-23 18:34:01 -0300

    Huawei

--- app/src/main/java/cba/night/MainActivity.java
+++ app/src/main/java/cba/night/MainActivity.java
@@ -4,21 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.awt.Button;
+
 
 import static cba.night.R.*;
 import static cba.night.R.id.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView fede = (TextView)findViewById(id.textView);
-    Button btn = (Button)
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
 
-        fede.setText("OCTA PERRO");
+
     }
 }
