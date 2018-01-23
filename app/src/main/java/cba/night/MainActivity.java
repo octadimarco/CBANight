bb4e682d41e56346fc30f6a65038acef1bb7adf3
Fede <federicoambroggio@gmail.com>
2018-01-23 18:03:57 -0300

    Octa

--- app/src/main/java/cba/night/MainActivity.java
+++ app/src/main/java/cba/night/MainActivity.java
@@ -2,12 +2,22 @@ package cba.night;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.awt.Button;
+
+import static cba.night.R.*;
+import static cba.night.R.id.*;
 
 public class MainActivity extends AppCompatActivity {
+
+  TextView fede = (TextView)findViewById(id.textView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
+
+        fede.setText("OCTA PERRO");
     }
 }
